2ab45f930276d355494dfb837e726549a5443347
ansfrzn <anisfarzana6012@gmail.com>
2022-01-25 10:14:14 +0800

    dateavailability

---
+++ src/java/DAO/DateAvailabilityDAO.java
@@ -0,0 +1,9 @@
+
+package DAO;
+
+
+import java.sql.Date;
+
+public interface DateAvailabilityDAO {
+    public void UpdateDateAvailibity(int id, Date startDate, Date endDate);
+}
---
+++ src/java/DAO/DateAvailabilityDAOImpl.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DBUtility.DBConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+//import java.util.Date;
+import java.sql.Date;
+
+
+public class DateAvailabilityDAOImpl implements DateAvailabilityDAO{
+
+    Connection conn;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+
+
+    @Override
+    public void UpdateDateAvailibity(int id, Date startDate, Date endDate) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    try{
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eventhallbookingsystem", "root", "");
+            
+            String sqlupdate = "update dateavailability set startDate=? set endDate=? where id=?";
+            
+            PreparedStatement ps = conn.prepareStatement(sqlupdate);
+            
+            ps.setInt(1, id);
+            ps.setDate(2, startDate);
+            ps.setDate(3, endDate);
+            ps.executeUpdate();
+            
+            conn.close();
+            
+            
+        }
+        catch(Exception ex){}
+    }
+    
+    }
+ 
